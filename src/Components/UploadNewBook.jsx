3171e9379c7cc52d20c9235fc27eeadd5fcbada9
Girish2906 <girishbhargava9202@gmail.com>
2025-03-13 12:43:34 +0530

    Upload Book Functionality

--- src/Components/UploadNewBook.jsx
+++ src/Components/UploadNewBook.jsx
@@ -9,9 +9,37 @@ import { BASE_URL } from "../utils/constants";
 const UploadNewBook = () => {
   const { isLogin, setIsLogin, showLoginAnimation, setShowLoginAnimation } =
     useExploreLogin();
-  const [genres , setGenres] = useState([]) ; 
   const [selectedGenres, setSelectedGenres] = useState([]);
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [name , setName] = useState('') ; 
+  const [author , setAuthor] = useState('') ; 
+  const [genres , setGenres] = useState([]) ; 
+  const [pages , setPages] = useState() ; 
+  const [price , setPrice] = useState() ; 
+  const [allFieldsMandatory , setAllFieldsMandatory] = useState(false) ; 
+  const [coverImage , setCoverImage] = useState() ; 
+  let fileBase64 = '' ; 
+
+  const handleFileUpload = async (event) => {
+    const file = event.target.files[0]; // Get the selected file
+  
+    if (file) {
+      const base64 = await convertToBase64(file);
+      console.log(base64) ; 
+      fileBase64 = base64 ; 
+    }
+  };
+  
+  // Function to convert file to Base64
+  const convertToBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result.split(",")[1]); // Get only Base64 part
+      reader.onerror = (error) => reject(error);
+    });
+  };
+  
 
   const fetchGenres = async () => {
     const response = await axios.get(BASE_URL + '/book/genres' , {withCredentials: true} ) ; 
@@ -22,6 +50,31 @@ const UploadNewBook = () => {
   useEffect(() =>{
     fetchGenres() ; 
   } , []) ; 
+
+  const newBook = async () => {
+    console.log("new book" , name , author , pages , price  , fileBase64, genres ) ; 
+    if(!name || !author || !pages || !price || !selectedGenres.length || !fileBase64.length ){
+      setAllFieldsMandatory(true) ; 
+      setTimeout(() => {
+        setAllFieldsMandatory(false) ; 
+      } , 2000) ; 
+    } else{
+      const formData = new FormData() ; 
+      formData.append('name' , name) ; 
+      formData.append('author' , author) ; 
+      formData.append('pages'  , pages) ; 
+      formData.append('genre' , selectedGenres) ; 
+      formData.append('price' , price) ; 
+      formData.append('image' , fileBase64) ; 
+      const response = await axios.post(BASE_URL + '/upload/Book' , formData , {withCredentials: true}) ; 
+      console.log("this is the coming response: ",response) ; 
+    }
+  }
+
+  const fileCheck = (event) => {
+    console.log(event.target , event.target.files)
+    console.log("filecheck fuynction")
+  }
 
 
   const toggleGenre = (genre) => {
@@ -38,12 +91,12 @@ const UploadNewBook = () => {
           <div className="space-y-4">
             <div className="flex items-center">
               <label className="w-24 text-gray-100">Name</label>
-              <input type="text" className="input input-bordered w-full" />
+              <input className="input input-bordered w-full" type="text"  value={name}  onChange={(e) => {setName(e.target.value)}} />
             </div>
 
             <div className="flex items-center">
               <label className="w-24 text-gray-100">Author</label>
-              <input type="text" className="input input-bordered w-full" />
+              <input type="text" className="input input-bordered w-full"  value={author}  onChange={(e) => {setAuthor(e.target.value)}} />
             </div>
 
             <div className="flex items-center relative">
@@ -78,16 +131,25 @@ const UploadNewBook = () => {
 
             <div className="flex items-center">
               <label className="w-24 text-gray-100">Pages</label>
-              <input type="number" className="input input-bordered w-full" />
+              <input type="number" className="input input-bordered w-full"  value={pages}  onChange={(e) => {setPages(e.target.value)}} />
             </div>
 
             <div className="flex items-center">
               <label className="w-24 text-gray-100">Price</label>
-              <input type="number" className="input input-bordered w-full" />
+              <input type="number" className="input input-bordered w-full"  value={price}  onChange={(e) => {setPrice(e.target.value)}} />
+            </div>
+            <div className="flex items-center">
+              <label className="w-24 text-gray-100">Cover</label>
+              <input type="file" className="file-input" onChange={handleFileUpload} />
             </div>
           </div>
+         <div className="flex justify-center">
+          {
+            allFieldsMandatory &&   <p className="text-red-500">All fields are mandatory</p>
+          }
+         </div>
           <div className="flex justify-center mt-4">
-            <button className="btn btn-primary">Save</button>
+            <button className="btn btn-primary" onClick={ newBook  }>Save</button>
           </div>
         </div>
       </div>
